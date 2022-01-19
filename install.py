dfeab766a9bc42335bf65da54c452d43f7781ea0
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2022-01-19 13:17:41 -0600

    - log source binary locations - use '-onedir' option

--- install.py
+++ install.py
@@ -3,10 +3,17 @@ import PyInstaller.__main__
 import os
 from ffmpegio.ffmpeg import FFMPEG_BIN, FFPROBE_BIN
 
-add_ffmpeg = f'{FFMPEG_BIN}{os.pathsep}bin'
-add_ffprobe = f'{FFPROBE_BIN}{os.pathsep}bin'
+from PyInstaller import log as logging
 
-onefile_or_ondir = '--onefile' if 1 else '--onedir'
+logger = logging.getLogger(__name__)
+
+logger.info(f"Path of the FFmpeg binary: {FFMPEG_BIN}")
+logger.info(f"Path of the FFprobe binary: {FFPROBE_BIN}")
+
+add_ffmpeg = f"{FFMPEG_BIN}{os.pathsep}bin"
+add_ffprobe = f"{FFPROBE_BIN}{os.pathsep}bin"
+
+onefile_or_ondir = "--onefile" if 0 else "--onedir"
 
 PyInstaller.__main__.run(
     [
