f9b569fb5db911690e633cf8b83bc0d3b07a16d9
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2022-01-19 13:18:43 -0600

    - accepts folder paths as input - improved progress display

--- dev/fluorofix.py
+++ dev/fluorofix.py
@@ -1,17 +1,24 @@
 import sys
-from os import path
+from os import path, walk, getcwd
 from configure import readOptionJSON
 from transcode import transcode
-from tqdm import tqdm
+
+# from tqdm import tqdm
 import ffmpegio
+
+# print(f"current dir: {getcwd()}")
 
 if not getattr(sys, "frozen", False):
     print("non-exe test run")
     sys.argv = [
-        "fluorofix",
-        "data\\480p.mp4",
-        "data\\1080p.mp4",
+        sys.executable,
+        # "data\\280\\Pt280_Visit1.avi",
+        "C:\\Users\\Takeshi Ikuma\\Documents\\python-fluorofix\\data\\1543",
+        # "data\\480p.mp4",
+        # "data\\1080p.mp4",
     ]
+
+print(f"argv: {sys.argv}")
 
 # read the config file
 if path.basename(sys.executable).startswith("python"):
@@ -34,22 +41,28 @@ if nfiles == 0:
     sys.exit(0)
 
 # scan for json file in the input
-vid_files, json_files = [], []
+vid_files, json_files = set(), set()
+
 for x in sys.argv:
-    (vid_files, json_files)[x.endswith(".json")].append(x)
+    if path.isdir(x):
+        for y in [path.join(p, f) for p, _, fs in walk(x) for f in fs]:
+            (vid_files, json_files)[y.endswith(".json")].add(y)
+    else:
+        (vid_files, json_files)[x.endswith(".json")].add(x)
 
 if len(json_files):
-    opts = readOptionJSON(json_files[-1], opts, update_file=False)
+    opts = readOptionJSON(tuple(json_files)[-1], opts, update_file=False)
 
 n = len(vid_files)
 fail_msg = "failed" if opts["Overwrite"] else "failed or file already exists"
-with tqdm(total=n) as pbar:
-    for i, file in enumerate(vid_files):
-        try:
-            pbar.set_description(path.basename(file))
-            pbar.update(i)
-            transcode(file, opts)
-        except Exception as err:
-            pass  # print(f"({i}/{n}) {file}... {fail_msg}")
+# with tqdm(total=n) as pbar:
+for i, file in enumerate(vid_files):
+    print(f"({i+1}/{n}) {file}...")
+    try:
+        # pbar.set_description(path.basename(file))
+        # pbar.update(i)
+        transcode(file, opts)
+    except Exception as err:
+        print(f"   skipping: {err}")
 
 input("\nPress Enter to Exit...")
